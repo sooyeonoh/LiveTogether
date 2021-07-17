eee7f6e292aad09773bc31083ba92a3f6860a42a
Sooyeon <sooyeonoh48@gmail.com>
2021-07-17 15:02:21 -0700

    Add total grocery price

--- client/src/components/Groceries/Groceries.js
+++ client/src/components/Groceries/Groceries.js
@@ -10,12 +10,15 @@ function Groceries(props) {
     const userID = props.match.params.id;
 
     const [groceries, setGroceries] = useState([]);
+    const [price, setPrice] = useState(0);
 
     useEffect(() => {
         const fetch = () => {
             axios.get("http://localhost:5000/groceries/" + userID, axiosConfig).then(res => {
                 setGroceries(res.data);
-                console.log(res.data);
+                res.data.forEach(g => {
+                    setPrice(prevPrice => (parseFloat(prevPrice) + parseFloat(g.cost)).toFixed(2))
+                })
             }).catch((error) => {
                 console.log(error)
             });
@@ -25,11 +28,15 @@ function Groceries(props) {
 
     function updateGroceryList(list) {
         setGroceries(list)
+        list.forEach(g => {
+            setPrice(prevPrice => (parseFloat(prevPrice) + parseFloat(g.cost)).toFixed(2))
+        })
     }
 
     function removeGrocery(grocery) {
         axios.post("http://localhost:5000/removeGrocery", grocery, axiosConfig).then(res => {
             setGroceries(res.data);
+            setPrice(prevPrice => (parseFloat(prevPrice) - parseFloat(grocery.cost)).toFixed(2));
             console.log("Successfully removed grocery")
         }).catch(err => {
             console.log(err);
@@ -41,15 +48,22 @@ function Groceries(props) {
         <div className="view">
             <NavBar userID={userID} history={props.history}/>
             <div className="main-padding d-flex justify-content-between align-items-start" >
-                <Search updateGroceryList={setGroceries}/>
-                <div className='w-100 h-100 rounded' style={{marginLeft: '2em', backgroundColor: "white", padding: '35px'}}>
-                    <h3>Your Groceries</h3>
-                    <div className="d-flex flex-wrap" style={{overflow: 'scroll'}}>
-                        {groceries.map(g => 
-                            <GroceryItem groceryItem={g} remove={removeGrocery} key={groceries.indexOf(g)} />
-                        )}
+                <Search updateGroceryList={updateGroceryList}/>
+                <div className='panel d-flex flex-column justify-content-between' style={{marginLeft: '2em'}}>
+                    <div>
+                        <h3>Your Groceries</h3>
+                        <div className="d-flex flex-wrap" style={{overflow: 'scroll', marginTop: '15px'}}>
+                            {groceries.map(g => 
+                                <GroceryItem groceryItem={g} remove={removeGrocery} key={groceries.indexOf(g)} />
+                            )}
+                        </div>
                     </div>
-                    
+                    <div className="d-flex justify-content-end">
+                        <button className="border-0 d-flex justify-content-between align-items-center" style={{paddingLeft: '1.5rem', paddingRight: '1.5rem', width: '255px',height: '50px', color: 'white', backgroundColor: '#fb6c70', borderRadius: '20px'}}>
+                            <div style={{fontSize: '18px'}}> Estimated Total </div>
+                            <div style={{fontSize: '18px', fontWeight: 'bolder'}}> ${price} </div>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
--- client/src/components/Groceries/GroceryItem.js
+++ client/src/components/Groceries/GroceryItem.js
@@ -4,8 +4,10 @@ import CancelIcon from '@material-ui/icons/Cancel';
 
 const GroceryItem = (props) => (
         <Card className="border-0 m-2 p-1 pt-4" style={{ width: '180px', boxShadow: '1px 1px 5px rgba(0, 0, 0, .15)'}}>
-            <Card.Img className="m-auto" style={{ maxWidth: '75px', maxHeight: '75px' }} variant="top" src={"https://spoonacular.com/cdn/ingredients_100x100/"+ props.groceryItem.image} />
-            <CancelIcon type="button" onClick={() => props.remove(props.groceryItem)} className="position-absolute" style={{top: '0', right: '0', margin: '10px 10px 0 0', color: 'lightgray'}}/>
+            <div className="m-auto" style={{ width: '75px', height: '75px' }}>
+                <Card.Img style={{ maxWidth: '75px', maxHeight: '75px' }} variant="top" src={"https://spoonacular.com/cdn/ingredients_100x100/"+ props.groceryItem.image} />
+            </div>
+            <CancelIcon type="button" id="buttonicon" onClick={() => props.remove(props.groceryItem)} className="position-absolute" style={{top: '0', right: '0', margin: '10px 10px 0 0'}}/>
                 <Card.Body>
                     <Card.Title>{props.groceryItem.name}</Card.Title>
                     <Card.Text className="d-flex justify-content-between">
